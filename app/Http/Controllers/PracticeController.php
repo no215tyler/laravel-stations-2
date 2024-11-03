33ac22c14791bdc25f75a33106dab9c2961073e3
no215tyler <no.215.tyler@gmail.com>
2024-11-03 19:29:03 +0900

    Station02: RouteとControllerをどちらも使ってみよう

---
+++ app/Http/Controllers/PracticeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PracticeController extends Controller
+{
+    public function sample() {
+      return response('practice');
+    }
+
+    public function sample2() {
+      $test = 'practice2';
+      return response($test);
+    }
+
+    public function sample3() {
+      return response("test");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('practice', function() {
-  return response('practice');
-});
+use App\Http\Controllers\PracticeController;
 
-Route::get('practice2', function() {
-  $test = 'practice2';
-return response($test);
-});
-
-Route::get('practice3', function() {
-  return response('test');
-});
+// Route::get('URL', [Controllerの名前::class, 'Controller内のfunction名']);
+Route::get('/practice', [PracticeController::class, 'sample']);
+Route::get('/practice2', [PracticeController::class, 'sample2']);
+Route::get('/practice3', [PracticeController::class, 'sample3']);
